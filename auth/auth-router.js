a23eb1ffe8622db16c9be74b7e3fef9b81112521
Brenda Joshua <brendajosh.bj@gmail.com>
2019-11-16 22:38:03 +0300

    created auth-router

---
+++ auth/auth-router.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model');
+const secrets = require('../config/secrets');
+const router = express.Router();
+
+router.post('/register', (req, res) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+    .then(saved => {
+        res.status(201).json(saved);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'This username is already taken' });
+    });
+});
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    Users.findBy ({ username })
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+            res.status(200).json({ token });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    });
+});
+
+function generateToken(user) {
+    const payload = {
+        username: user.username
+    };
+    const options = {
+        expiresIn: '1d',
+    };
+    return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+module.exports = router;
