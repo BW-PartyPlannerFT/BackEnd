3f66d3f1fb5b6c7464a65ae5fbbb65bd45d5cea4
Brenda Joshua <brendajosh.bj@gmail.com>
2019-11-17 01:12:29 +0300

    added pictures model and router

---
+++ pictures/pictures-router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./pictures-model');
+
+router.get('/', (req, res) => {
+  db.get()
+  .then(pictures => {
+    res.json(pictures);
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to get pictures' });
+  });
+});
+
+
+module.exports = router;
